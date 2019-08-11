e4eb771035cf04ea875d709923a1400175317474
qqexwed3 <59160935@go.buu.ac.th>
2019-08-11 19:47:19 +0700

    Finish RandomNumber

--- app/src/main/java/buu/informatics/s59160935/diceroller/MainActivity.kt
+++ app/src/main/java/buu/informatics/s59160935/diceroller/MainActivity.kt
@@ -3,7 +3,9 @@ package buu.informatics.s59160935.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,9 +14,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
+
+        val resultText: TextView = findViewById(R.id.result_text)
+        resultText.text = "Dice Rolled!"
+
     }
 
     private fun rollDice() {
+        val randomInt = Random().nextInt(6) + 1
+        val resultText: TextView = findViewById(R.id.result_text)
+        resultText.text = "Dice Rolled!"
+        resultText.text = randomInt.toString()
         Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
     }
 }
